fcb647360e8e016a6b867cf1f234ae9bb44de14c
haku7a <v.a.neyaskin@gmail.com>
2026-02-19 06:40:16 +0300

    feat: add info app and initial navigation features

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "dashboard.apps.DashboardConfig",
+    "info.apps.InfoConfig",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
--- dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from dashboard.views import index
-
-app_name = "dashboard"
-
-urlpatterns = [
-    path("", index, name="index"),
-]
